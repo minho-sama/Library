3134a279e74505873d067b7664d31747b0f92cdb
minho-sama <n.a.minh1106@gmail.com>
2021-04-03 15:02:44 +0200

    added books

--- script.js
+++ script.js
@@ -18,8 +18,9 @@ const inputs = document.querySelectorAll('.input')
 const titleInput = document.querySelector('#titleInput')
 const authorInput = document.querySelector('#authorInput')
 const pagesInput = document.querySelector('#pagesInput')
-
 const submitBook = document.querySelector('#submit')
+
+const books = document.querySelector('.books')
 
 let myLibrary = [];
 
@@ -30,18 +31,86 @@ function Book (title, author, pages, finished){
     this.finished = finished
 }
 
+let checkedValue = document.getElementById("isFinished")
+
+function checked (){
+    return (checkedValue.checked) ? true : false;
+}
+
 function addBookToLibrary (){
+    clearBooks()
+
     let title = titleInput.value
     let author = authorInput.value
     let pages = pagesInput.value
-    let finished = prompt('hey', "")
+    
+    let finished = checked()
 
     let newBook = new Book(title, author, pages, finished)
     myLibrary.push(newBook)
+    
+    print()
 
+    clearFields()
+}
+
+submitBook.addEventListener('click', addBookToLibrary)
+
+
+function print(){
+    // let lastBook = myLibrary[myLibrary.length - 1]
+    //     book = document.createElement('div')
+    //     book.textContent = `title: ${lastBook.title} 
+    //                         author: ${lastBook.author}
+    //                         pages: ${lastBook.pages}
+    //                         ${lastBook.finished}` 
+    //     book.classList.add('bookStyle')
+    //     books.appendChild(book)
+    // így nem kéne clearelni mindent submit book előtt, de a delete-nél üres helyek maradnának
+
+    for (const book of myLibrary){
+        let cover = document.createElement('div')
+        cover.textContent =`title: ${book.title} 
+                            author: ${book.author}
+                            pages: ${book.pages}
+                            ${book.finished}` 
+        cover.classList.add('bookStyle')
+        books.appendChild(cover)
+    }
+    const addedBooks = document.querySelectorAll('.bookStyle')
+    addedBooks.forEach(book => {
+        let del = document.createElement('BUTTON');
+        del.classList.add('delBtn')
+        del.textContent = 'x'
+        book.appendChild(del)
+    }) 
+    //delete gomb event targetttel?
+}
+
+function clearBooks(){
+    books.textContent = '';
+}
+
+
+function clearFields(){
     titleInput.value = ''
     authorInput.value =''
     pagesInput.value = ''
 }
 
-submitBook.addEventListener('click', addBookToLibrary)
+/*array:
+myLibrary = [
+    Book:{
+        author:
+        title:
+        pages:
+        finished: true
+    },
+        Book:{
+        author:
+        title:
+        pages:
+        finished: false
+    },
+]
+*/
